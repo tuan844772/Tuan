fc3ae588765541d3d84641f5de44eca4c4aa0a9e
le van giap <giap.le@pamoible.co>
2020-03-13 14:52:07 +0700

    +

---
+++ lib/componets/horizontal_listview.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+class HorizontalList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      height: 100.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Category(
+            image_location: 'assets/images/jpg/AAIPESM.jpeg',
+            image_caption: 'shirt',
+          ),
+          Category(
+            image_location: 'assets/images/jpg/AAIPESM.jpeg',
+            image_caption: 'dress',
+          ),
+          Category(
+            image_location: 'assets/images/jpg/AAIPESM.jpeg',
+            image_caption: 'pants',
+          ),
+          Category(
+            image_location: 'assets/images/jpg/AAIPESM.jpeg',
+            image_caption: 'formal',
+          ),
+          Category(
+            image_location: 'assets/images/jpg/AAIPESM.jpeg',
+            image_caption: 'shoes',
+          ),
+          Category(
+            image_location: 'assets/images/jpg/AAIPESM.jpeg',
+            image_caption: 'shirt',
+          ),
+        ],
+      ),
+    );
+  }
+}
+class Category extends StatelessWidget{
+  final String image_location;
+  final String image_caption;
+  Category({
+    this.image_location,
+    this.image_caption
+});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(padding: const EdgeInsets.all(2.0),
+    child: InkWell(onTap: (){},
+    child: Container(
+      width: 100.0,
+      child: ListTile(
+        title: Image.asset(image_location,
+        width: 100.0,
+        height: 80.0,),
+        subtitle: Container(
+          alignment: Alignment.topCenter,
+          child: Text(image_caption),
+        )
+    ),
+    ),
+    ),
+    );
+  }
+
+}
